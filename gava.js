3a0f04d19e6177693326f21a6fabc2c3ce60b796
Salah Abubkar <salahabubkar96@gmail.com>
2023-01-24 21:33:54 +0400

    first commit

---
+++ gava.js
@@ -0,0 +1,14 @@
+let x = document.querySelector('.x');
+
+let container = document.querySelector('.container')
+
+function phons(phon){
+    x.src= phon;
+
+}
+
+function colors(color){
+    container.style.background = color;
+
+
+}
